838655b968a210e11dbcfe7c8359bfed29a57593
ellianorsX <marjonathe98@icloud.com>
2026-05-02 10:48:31 +0900

    feat: FrontEnd java scriptni qurish

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const mongodb = require("mongodb");
 
 let db;
 const connectionString =
-  "mongodb://elliasX:alliasmongo2026@ac-vfejyfe-shard-00-00.0jctfcc.mongodb.net:27017,ac-vfejyfe-shard-00-01.0jctfcc.mongodb.net:27017,ac-vfejyfe-shard-00-02.0jctfcc.mongodb.net:27017/Reja?ssl=true&replicaSet=atlas-hpr7d7-shard-0&authSource=admin&appName=Cluster0";
+  "mongodb+srv://elliasX:alliasmongo2026@cluster0.0jctfcc.mongodb.net/Reja";
+
 mongodb.connect(
   connectionString,
   {
